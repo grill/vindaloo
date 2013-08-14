184eaf26b9a7e0c222af04b685b662528e1daa12
gabriel <gab.grill@gmail.com>
2013-08-14 11:33:04 +0200

    recommit of first draft

--- vindaloo/Parser.fs
+++ vindaloo/Parser.fs
@@ -1,10 +1,75 @@
-﻿//namespace vindaloo
-
-module Vindaloo.Parser
+﻿module Vindaloo.Parser
 
 open FParsec
 
-let one = 1
+let ws = spaces
+let str = pstring
+
+//Literals: literal --> 0# | 1# | ...
+let literal : Parser<uint64, unit> = puint64 .>> str "#"
+
+//Primitive ops: prim --> +# | -# | *# | /#
+let prim : Parser<string, unit> = str "+#" <|> str "-#" <|> str "*#" <|> str "/#"
+
+
+let isAsciiIdStart = fun c -> isAsciiLetter c || c = '_'
+let var : Parser<string, unit> = identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart))
+
+//Variable lists: vars --> {var(1), ... , var(n)}    n >= 0
+let vars =  between (str "{") (str "}") (sepBy var (str ","))
+
+//Atom: atom --> var | literal
+let atom = var <|> literal
+
+//Atom lists: atoms --> {atom(1), ..., atom(n)}    n >= 0
+let atoms = between (str "{") (str "}") (sepBy atom (str ","))
+
+//Bindings: binds --> var(1) = lf(1); ... ; var(n) = lf(n)    n >= 1
+let bind = var .>> ws .>> str "=" .>> ws .>>. lf .>> ws
+let binds = bind .>>? str ";" .>>. sepBy bind (str ";")
+
+//Update flag: pi --> u | n
+let pi = str "u" <|> str "n"
+
+//TODO: FIXME
+let constr = str "constr"
+
+//Expression: expr --> let binds in expr
+//                  | letrec binds in expr
+//                  | case expr of alts
+//                  | var atoms
+//                  | constr atoms
+//                  | prim atoms
+//                  | literal
+let expr = (str "let" >>. binds .>> ws .>> str "in" .>> ws .>> expr) <|>
+    (str "letrec" >>. binds .>> ws .>> str "in" .>> ws .>> expr) <|>
+    (str "case" >>. expr .>> ws .>> str "of" .>> ws .>> alts) <|>
+    (var .>> ws .>> atoms) <|>
+    (constr >> ws >>. atoms) <|>
+    (prim .>> ws .>>. atoms) <|>
+    (literal)
+
+let ``->`` = ws .>> str "->" .>> ws
+
+//Lambda-forms: lf --> vars(f) \pi vars(a) -> expr
+let lf = vars .>> str "\\" .>>. pi .>>. vars .>> ``->`` .>>. expr
+
+//Default alt: default --> var -> expr
+//                      | default -> expr
+let dalt = (str "default" <|> var) .>> ``->`` .>>. expr
+
+//Primitive alt: palt --> literal -> expr
+let palt = literal .>> ``->`` .>>. expr
+
+//Algebraic alt: aalt --> constr vars -> expr
+let aalt = constr >>. vars .>> ``->`` .>>. expr
+
+//Alternatives: alts --> aalt(1); ... ; aalt(n); default    n >= 0 (Algebraic)
+//                   | palt(1); ... ; palt(n); default    n >= 0 (Primitive)
+let alts = (sepEndBy aalt (str ";")  <|> sepEndBy palt (str ";")) .>> ws .>>. dalt
+
+//Program: prog --> binds
+let prog = binds
 
 (** Syntax of the STG language **
 
--- vindaloo/ParserTest.fs
+++ vindaloo/ParserTest.fs
@@ -2,9 +2,50 @@
 
 module Vindaloo.Test.Parser
 
+open FParsec
 open Xunit
 open FsUnit.Xunit
 open Vindaloo.Parser
 
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let one = 1
+
 [<Fact>]
 let ``one is one`` () = one |> should equal 1
+
+[<Fact>]
+let ``two is two`` () = 2 |> should equal 2
+
+[<Fact>]
+let ``bla`` () = one |> should equal 1
+
+(*
+  add = \a b ->
+     case a of {
+         a -> case b of {
+             b -> primOp + a b
+         }
+     }
+*)
+
+
+(*
+  compose = \f g x ->
+     let gx = g x
+     in f gx
+*)
+
+(*
+    map = \f xs->
+        case xs of {
+            Cons x xs ->
+              let fx = f x
+              in let mapfxs = map f xs
+                 in Cons fx mapfxs
+            ; Nil -> Nil
+        }
+*)
